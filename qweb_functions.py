06b7b856e2f06e3ae6f898140ba3250f951ae243
Muhammad Rizwan <58412444+rizwanch786@users.noreply.github.com>
2022-11-03 15:18:32 +0500

    Qweb mostly used functions

---
+++ qweb_functions.py
@@ -0,0 +1,48 @@
+# from datetime import datetime
+# from pytz import timezone
+# from datetime import date
+
+
+# def string_slicing(s):
+#     return s[:15]
+
+
+# # Driver
+# string = 'Muhammad Ijaz'
+# print(string_slicing(string))
+
+
+# def get_date_format(date):
+#     return date.strftime("%d-%m-%Y")
+
+
+# def get_date_with_month(date):
+#     return date.strftime("%B %d, %Y")
+
+
+# # Driver
+# d = date.today()
+# print(get_date_format(d))
+# print(get_date_with_month(d))
+
+
+# # get amount in comma seperated and ending with 00
+
+
+# def get_amount(amount):
+#     txt = "{price:,.2f}"
+#     return txt.format(price=amount)
+
+
+# # Driver
+# amount = 458644444.3
+# print(get_amount(amount))
+
+
+# # Change Timezone
+# t = datetime.now()
+# print(t)
+# time = t.astimezone(timezone('Asia/Karachi'))
+# print(time)
+
+
